960137d60ed4d2d62d27444c2ade7634c32fc52f
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-16 13:59:42 +0530

    Update getRate.js

--- routes/customer/getRate.js
+++ routes/customer/getRate.js
@@ -6,7 +6,7 @@ router.post("/",(request,response)=>
     let query=`SELECT cloth_type,rate 
                FROM rate INNER JOIN cloth_type
                ON rate.cloth_type_id=cloth_type.cloth_type_id
-               WHERE society_id='${request.body.society_id}';`;
+               WHERE society_id in (select society_id from customer where customer_id=${request.body.customer_id})`;
     db.query(query,(err,res)=>{
         if (err != null) response.status(500).json({ error: err.message, success: false });
         if(res.length>0){response.status(200).json({result:res, success: true});}
