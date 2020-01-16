2d81c2d0ff9cc1fc965af330639f407b17710d1e
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-16 14:33:55 +0530

    Update getRate.js

--- routes/customer/getRate.js
+++ routes/customer/getRate.js
@@ -9,7 +9,7 @@ router.post("/",(request,response)=>
                WHERE society_id in (select society_id from customer where customer_id=${request.body.customer_id})`;
     db.query(query,(err,res)=>{
         if (err != null) response.status(500).json({ error: err.message, success: false });
-        if(res.length>0){response.status(200).json({result:res, success: true});}
+        if(res.length>0){response.status(200).json({result:res,message:"Rate For Your Society", success: true});}
         else {
             response.json({success:true,message:"Rate Not Updated For Your Society"});
         }
