c67cb7d4471f4b5268be581502b82816c98d5876
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-16 15:07:53 +0530

    Update getRate.js

--- routes/customer/getRate.js
+++ routes/customer/getRate.js
@@ -6,10 +6,12 @@ router.post("/",(request,response)=>
     let query=`SELECT cloth_type,rate 
                FROM rate INNER JOIN cloth_type
                ON rate.cloth_type_id=cloth_type.cloth_type_id
-               WHERE society_id in (select society_id from customer where customer_id=${request.body.customer_id})`;
+               WHERE society_id in (select society_id from customer where customer_id='${request.body.customer_id}')`;
     db.query(query,(err,res)=>{
-        if (err != null) response.status(500).json({ error: err.message, success: false });
-        if(res.length>0){response.status(200).json({result:res,message:"Rate For Your Society", success: true});}
+        if (err)
+        {response.status(500).json({ error: err.message, success: false });
+        
+         if(res.length>0){response.status(200).json({result:res,message:"Rate For Your Society", success: true});}
         else {
             response.json({success:true,message:"Rate Not Updated For Your Society"});
         }
