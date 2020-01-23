217313633cf92920cf37b9d3707ad4af799373bb
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 18:39:04 +0530

    Update order_det.js

--- routes/admin/order_detail/order_det.js
+++ routes/admin/order_detail/order_det.js
@@ -5,8 +5,7 @@ const verifyToken = require("../verifyToken");
 
 router.get("/", verifyToken, (req, res) => {
   let sql =
-    "SELECT  customer.customer_name,customer.phone,customer.flat_no,orders.picked_date,orders.total_count,society.society_name,wings.wings_name  FROM  orders, customer,society,wings 
-where orders.customer_id=customer.customer_id && customer.society_id=society.society_id && customer.wings_id=wings.wings_id && status=5";
+    "SELECT  customer.customer_name,customer.phone,customer.flat_no,orders.picked_date,orders.total_count,society.society_name,wings.wings_name  FROM  orders, customer,society,wings where orders.customer_id=customer.customer_id && customer.society_id=society.society_id && customer.wings_id=wings.wings_id && status=5";
   let query = db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
