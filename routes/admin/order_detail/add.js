511fc6eca0e50108910a3250861e4c15df3628d6
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 19:04:08 +0530

    Update add.js

--- routes/admin/order_detail/add.js
+++ routes/admin/order_detail/add.js
@@ -8,13 +8,24 @@ router.post("/", verifyToken, (req, res) => {
   }
   let { order_id, cloth_type_id, count } = req.body;
   if (order_id && cloth_type_id && count) {
-    let post = {
-      order_id: order_id,
-      cloth_type_id: cloth_type_id,
-      count: count
-    };
-    let sql = "INSERT INTO order_detail SET ?";
-    let query = db.query(sql, post, (err, result) => {
+//     let post = {
+//       order_id: order_id,
+//       cloth_type_id: cloth_type_id,
+//       count: count
+//     };
+//     let sql = "INSERT INTO order_detail SET ?";
+//     let query = db.query(sql, post, (err, result) => {
+    let post=`INSERT into order_detail(order_id,cloth_type_id,count) values ?`
+                  pool.query(post,[req.body.items.map(item =>[req.body.order_id,item.cloth_id,item.count])])
+                  .then((postResult)=>{
+                  if(postResult.affectedRows>0)
+                      {
+                        let orderQuery=`update orders set status=6 where order_id='${req.body.order_id}' `
+                        let timequery=`insert into time(order_id,status_id,timestamp) values(${req.body.order_id},6,timeNow())` 
+                        pool.query(orderQuery)}
+                    else{
+                      res.status(200).json({ msg: "no detail has been added" })
+                    }
       if (err) {
         console.log(err);
       } else {
@@ -27,3 +38,5 @@ router.post("/", verifyToken, (req, res) => {
 });
 
 module.exports = router;
+
+
