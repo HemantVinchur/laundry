5f944efeea96a26e9b3612c8889e77f2ecf38930
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 13:25:10 +0530

    Create get.js

---
+++ routes/admin/get.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const db = require("../db");
+dotenv.config();
+const verifyToken = require("./verifyToken");
+const router = express.Router();
+
+
+router.post("/", verifyToken, (req, res) => {
+  if (req.decoded.admin_type != "admin") {
+    return res.json({ msg: "Unauthorized" });
+  }
+  let getAdminQuery=`select name,mobile_no,address_1,address_2,city,admin_type from admin where admin_id in (${req.body.admin_id.toString()})`
+  db.query(getAdminQuery)
+  .then((result)=>{
+    if(result.length>0)
+    {
+      return res.json({success:true,result:result});
+    }
+    else{
+      return res.json({success:true,msg:"No admin found"});
+    }
+  })
+  .catch((error)=>{
+    if(error)
+    {
+      console.log(error);
+       return res.json({success:false,error:"Some Error Occured"});
+    }
+  })
+  })
