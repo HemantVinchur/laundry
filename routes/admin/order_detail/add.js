e808893a8e0fd5bd8ec1830c97931ba15d6a7fda
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 19:21:48 +0530

    Update add.js

--- routes/admin/order_detail/add.js
+++ routes/admin/order_detail/add.js
@@ -16,12 +16,12 @@ router.post("/", verifyToken, (req, res) => {
     //     let sql = "INSERT INTO order_detail SET ?";
     //     let query = db.query(sql, post, (err, result) => {
     let post = `INSERT into order_detail(order_id,cloth_type_id,count) values ?`
-    pool.query(post, [req.body.items.map(item => [req.body.order_id, item.cloth_id, item.count])])
+    db.query(post, [req.body.items.map(item => [req.body.order_id, item.cloth_id, item.count])])
       .then((postResult) => {
         if (postResult.affectedRows > 0) {
           let orderQuery = `update orders set status=6 where order_id='${req.body.order_id}' `
           let timequery = `insert into time(order_id,status_id,timestamp) values(${req.body.order_id},6,timeNow())`
-          pool.query(orderQuery)
+          db.query(orderQuery)
         }
         else {
           res.status(200).json({ msg: "no detail has been added" })
