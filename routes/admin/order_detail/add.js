d62bb6461e0a8c7e64c153c640c9e4cc65a52b01
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 19:49:09 +0530

    Update add.js

--- routes/admin/order_detail/add.js
+++ routes/admin/order_detail/add.js
@@ -15,24 +15,42 @@ router.post("/", verifyToken, (req, res) => {
     //     };
     //     let sql = "INSERT INTO order_detail SET ?";
     //     let query = db.query(sql, post, (err, result) => {
-    let post = `INSERT into order_detail(order_id,cloth_type_id,count) values ?`
-    db.query(post, [req.body.items.map(item => [req.body.order_id, item.cloth_id, item.count])])
-      .then((postResult) => {
-        if (postResult.affectedRows > 0) {
-          let orderQuery = `update orders set status=6 where order_id='${req.body.order_id}' `
-          let timequery = `insert into time(order_id,status_id,timestamp) values(${req.body.order_id},6,timeNow())`
-          db.query(orderQuery)
-        }
-        else {
-          res.status(200).json({ msg: "no detail has been added" })
-        }
-        if (err) {
-          console.log(err);
-        } else {
-          res.status(200).json({ msg: "order detail has been added" });
-        }
-      });
-  }); 
+  let post = `INSERT into order_detail(order_id,cloth_type_id,count) values ?`
+  db.query(post, [req.body.items.map(item => [req.body.order_id, item.cloth_id, item.count])], (err, postResult) => {
+    if (err) {
+      console.log(err);
+      res.status(200).json({ msg: "no detail has been added" })
+    }
+    else {
+      if (postResult.affectedRows > 0) {
+        let orderQuery = `update orders set status=6 where order_id='${req.body.order_id}' `
+        let timequery = `insert into time(order_id,status_id,timestamp) values(${req.body.order_id},6,timeNow())`
+        db.query(orderQuery,(error,orderQueryResult)=>{
+            if(error){
+              console.log(error);
+              return res.status(500).json({ msg: "Some server error" })
+            }
+            else{
+              db.query(timequery,(error,resultTimeQuery)=>{
+                 if(error)
+                 {
+                   console.log(error);
+                   return res.status(500).json({ msg: "Some server error" })
+                 }
+                 else{
+                   res.status(200).json({ msg: "order detail has been added" });
+                 } 
+              })
+            }
+        })
+        
+      }
+      else{
+        res.status(200).json({ msg: "no detail has been added" })
+      }
+    }
+  })
+});
 
 module.exports = router;
 
