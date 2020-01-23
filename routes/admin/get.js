2b0c30bc4cb6f50da2d95c9f7ef55e410f4ec074
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 16:41:07 +0530

    Update get.js

--- routes/admin/get.js
+++ routes/admin/get.js
@@ -10,10 +10,10 @@ router.post("/", verifyToken, (req, res) => {
   if (req.decoded.admin_type != "admin") {
     return res.json({ msg: "Unauthorized" });
   }
-  console.log(req.body.admin_type.toString());
-  console.log(req.body.admin_type);
-  console.log(`select admin_id,name,mobile_no,address_1,address_2,city,admin_type from admin where admin_type in (${req.body.admin_type})`)
-  let getAdminQuery=`select name,mobile_no,address_1,address_2,city,admin_type from admin where admin_type in (?)`
+  //console.log(req.body.admin_type.toString());
+  //console.log(req.body.admin_type);
+  //console.log(`select admin_id,name,mobile_no,address_1,address_2,city,admin_type from admin where admin_type in (${req.body.admin_type})`)
+  let getAdminQuery=`select admin_id,name,mobile_no,address_1,address_2,city,admin_type from admin where admin_type in (?)`
   db.query(getAdminQuery,[req.body.admin_type],(err,result)=>{
     if(err)
     {
