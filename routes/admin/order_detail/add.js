b86f0515eb79f0a3cf4093640b190428969aff36
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-23 19:20:28 +0530

    Update add.js

--- routes/admin/order_detail/add.js
+++ routes/admin/order_detail/add.js
@@ -6,36 +6,33 @@ router.post("/", verifyToken, (req, res) => {
   if (req.decoded.admin_type != "admin") {
     return res.json({ msg: "Unauthorized" });
   }
-//   let { order_id, cloth_type_id, count } = req.body;
-//   if (order_id && cloth_type_id && count) {
-//     let post = {
-//       order_id: order_id,
-//       cloth_type_id: cloth_type_id,
-//       count: count
-//     };
-//     let sql = "INSERT INTO order_detail SET ?";
-//     let query = db.query(sql, post, (err, result) => {
-    let post=`INSERT into order_detail(order_id,cloth_type_id,count) values ?`
-                  pool.query(post,[req.body.items.map(item =>[item.order_id,item.cloth_type_id,item.count])])
-                  .then((postResult)=>{
-                  if(postResult.affectedRows>0)
-                      {
-                        let orderQuery=`update orders set status=6 where order_id='${req.body.order_id}' `
-                        let timequery=`insert into time(order_id,status_id,timestamp) values(${req.body.order_id},6,timeNow())` 
-                        pool.query(orderQuery)}
-                    else{
-                      res.status(200).json({ msg: "no detail has been added" })
-                    }
-      if (err) {
-        console.log(err);
-      } else {
-        res.status(200).json({ msg: "order detail has been added" });
-      }
-    });
-  } else {
-    res.json({ msg: "All fields are required" });
-  }
-);
+  // let { order_id, cloth_type_id, count } = req.body;
+  // if (order_id && cloth_type_id && count) {
+    //     let post = {
+    //       order_id: order_id,
+    //       cloth_type_id: cloth_type_id,
+    //       count: count
+    //     };
+    //     let sql = "INSERT INTO order_detail SET ?";
+    //     let query = db.query(sql, post, (err, result) => {
+    let post = `INSERT into order_detail(order_id,cloth_type_id,count) values ?`
+    pool.query(post, [req.body.items.map(item => [req.body.order_id, item.cloth_id, item.count])])
+      .then((postResult) => {
+        if (postResult.affectedRows > 0) {
+          let orderQuery = `update orders set status=6 where order_id='${req.body.order_id}' `
+          let timequery = `insert into time(order_id,status_id,timestamp) values(${req.body.order_id},6,timeNow())`
+          pool.query(orderQuery)
+        }
+        else {
+          res.status(200).json({ msg: "no detail has been added" })
+        }
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).json({ msg: "order detail has been added" });
+        }
+      });
+  }); 
 
 module.exports = router;
 
