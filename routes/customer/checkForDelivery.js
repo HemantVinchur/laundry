2685c4f2c997e317b7cf161e642cad6e8d3cb7fd
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:39:13 +0530

    bug fix

--- routes/customer/checkForDelivery.js
+++ routes/customer/checkForDelivery.js
@@ -6,7 +6,7 @@ router.post("/",(request,response)=>
     let query=`SELECT status FROM orders WHERE customer_id='${request.body.customer_id}' AND status='14'`;
     db.query(query,(err,res)=>{
         if (err != null) return response.status(500).json({ error: err.message , success: false});
-        console.log(res);
+//         console.log(res);
         
         if(res.length>0)
         {
