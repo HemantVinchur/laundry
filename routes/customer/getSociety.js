fbe239670638e1389af3ea3b9d4c6ad6267024e3
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:35:43 +0530

    bug fix

--- routes/customer/getSociety.js
+++ routes/customer/getSociety.js
@@ -6,12 +6,12 @@ router.post("/",(request,response)=>
     let society=`SELECT society_id,society_name FROM society`;
     db.query(society,(err,res)=>
     {
-        if (err != null) response.status(500).json({ error: err.message, success:false});
+        if (err != null) return response.status(500).json({ error: err.message, success:false});
         if(res.length>0){
-        response.status(200).json({result:res,success:true});
+        return response.status(200).json({result:res,success:true});
         }
         else{
-            response.json({message:"Society Doesn't Exist",success:true});
+            return response.json({message:"Society Doesn't Exist",success:true});
         }
     });
 
