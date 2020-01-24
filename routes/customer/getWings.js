c251ef57fd29517ba264c63d92e5a5d72e5a93fe
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:26:42 +0530

    bug fix

--- routes/customer/getWings.js
+++ routes/customer/getWings.js
@@ -6,7 +6,7 @@ router.post("/",(request,response)=>
     let wings=`SELECT wings_id,wings_name FROM wings WHERE society_id='${request.body.society_id}';`;
     db.query(wings,(err,res)=>
     {
-        if (err != null) response.status(500).json({ error: err.message,success:false });
+        if (err != null) return response.status(500).json({ error: err.message,success:false });
         if(res.length>0){
         response.status(200).json({result:res,success:true});
         }
