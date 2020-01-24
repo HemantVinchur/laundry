926b9f4b9a70624e8d506cb1d8709e16cca0d201
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:48:47 +0530

    major bug fix

--- routes/customer/callForLaundry.js
+++ routes/customer/callForLaundry.js
@@ -6,9 +6,9 @@ router.post("/",(request,response)=>
 
     let checkQuery=`SELECT order_id FROM orders WHERE status IN('0','1') AND customer_id='${request.body.customer_id}'`;
     db.query(checkQuery,(error,result)=>{
-        if(error) return response.status(500).json({ error: err.message , success: false});
+        if(error) return response.status(500).json({ error: error.message , success: false});
         else{
-            console.log(result);
+//             console.log(result);
             
             if(result.length>0)
             {
@@ -27,15 +27,15 @@ router.post("/",(request,response)=>
                     let timestamp=date.getFullYear()+'-'+month+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
                     let subquery=`INSERT INTO time(order_id,status_id,timestamp) VALUES
                                   ('${res.insertId}','0','${timestamp}')  `;
-                                  console.log(subquery);
+//                                   console.log(subquery);
                                   
-                    db.query(subquery,(err,res)=>{
-                        if (err != null) response.status(500).json({ error: err.message , success: false});
-                        response.json({success:true,message:"order created successfully"});
+                    db.query(subquery,(error2,result2)=>{
+                        if (error2 != null) return response.status(500).json({ error: error2.message , success: false});
+                        return response.json({success:true,message:"order created successfully"});
                     })    
                 }
                 else{
-                    response.json({message : "Order Not Created",success:false});
+                    return response.json({message : "Order Not Created",success:false});
                 }
                 });
                 
