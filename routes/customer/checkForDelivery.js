d5777d143351c7b999a52f95f742578d9133d870
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:15:03 +0530

    bug fix

--- routes/customer/checkForDelivery.js
+++ routes/customer/checkForDelivery.js
@@ -5,15 +5,15 @@ router.post("/",(request,response)=>
 {
     let query=`SELECT status FROM orders WHERE customer_id='${request.body.customer_id}' AND status='14'`;
     db.query(query,(err,res)=>{
-        if (err != null) response.status(500).json({ error: err.message , success: false});
+        if (err != null) return response.status(500).json({ error: err.message , success: false});
         console.log(res);
         
         if(res.length>0)
         {
-        response.status(200).json({success: true,message:"can call for laundry"});
+         return response.status(200).json({success: true,message:"can call for laundry"});
         }
         else{
-            response.status(200).json({success: true,message:"cannot call for laundry"});
+            return response.status(200).json({success: true,message:"cannot call for laundry"});
         }
     });
 });
