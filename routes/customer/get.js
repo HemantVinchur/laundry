f7f9a9cb9d619f034d6fad53d80f0f38103e10da
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:34:11 +0530

    bug fix

--- routes/customer/get.js
+++ routes/customer/get.js
@@ -10,13 +10,13 @@ router.post("/",(request,response)=>
                INNER JOIN wings ON wings.society_id=society.society_id
                WHERE customer.customer_id='${id}';`;
     db.query(query,(err,res)=>{
-        if (err != null) response.status(500).json({ error: err.message , success:false});
-        console.log(res);
+        if (err != null) return response.status(500).json({ error: err.message , success:false});
+//         console.log(res);
         
         if(res.length>0)
-        {response.status(200).json({result:res,success:true});}
+        { return response.status(200).json({result:res,success:true});}
         else
-        response.json({success:true,message:"customer not found"});
+        return response.json({success:true,message:"customer not found"});
     });           
 });
     module.exports=router;
