27f4b85261ab78b2fceb9749147ba3b05c0f5503
Samyak jain <samyak915jain@gmail.com>
2020-01-24 08:32:06 +0530

    bug fix

--- routes/customer/customer.js
+++ routes/customer/customer.js
@@ -1,45 +1,45 @@
 var express = require("express");
 var router = express.Router();
-//done
+
 var signup = require("./signup");
 router.use("/signup", signup);
-//done
+
 var get=require("./get");
 router.use("/get",get);
-//done
+
 var checkUser=require("./checkUser");
 router.use("/checkUser",checkUser);
-//done
+
 var getOutstanding=require("./getOutstanding");
 router.use("/getOutstanding",getOutstanding);
-//not done
+
 // var getPoints=require("./getPoints");
 // router.use("/getPoints",getPoints);
-//done
+
 var getWings=require("./getWings");
 router.use("/getWings",getWings);
-//done
+
 var getSociety=require("./getSociety");
 router.use("/getSociety",getSociety);
-//done
+
 var getHistory=require("./getHistory");
 router.use("/getHistory",getHistory);
-//done
+
 var getStatus=require("./getStatus");
 router.use("/getStatus",getStatus);
-//done
+
 var getRate=require("./getRate");
 router.use("/getRate",getRate);
-//done
+
 var updateStatus=require("./updateStatus");
 router.use("/updateStatus",updateStatus);
-//done
+
 var checkForDelivery=require("./checkForDelivery");
 router.use("/checkForDelivery",checkForDelivery);
-//done
+
 var callForLaundry=require("./callForLaundry");
 router.use("/callForLaundry",callForLaundry);
-//not done
+
 var payment=require("./payment");
 router.use("/payment",payment);
 
