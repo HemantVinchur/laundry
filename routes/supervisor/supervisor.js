a30340a36239dca3fefa87744cbaa7ffb970dea8
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-25 17:02:22 +0530

    Update supervisor.js

--- routes/supervisor/supervisor.js
+++ routes/supervisor/supervisor.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-var deliveryList = require("./deliveryList");
-router.use("/deliveryList", deliveryList);
+// var deliveryList = require("./deliveryList");
+// router.use("/deliveryList", deliveryList);
+
+var operations = require("./operations");
+router.use("/operations", operations);
+
 
 module.exports= router;
