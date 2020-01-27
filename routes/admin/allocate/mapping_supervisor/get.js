19c16d770336beb55a48ceda749da2bb9795e115
Omkar Lanjekar <52631363+lanomkar@users.noreply.github.com>
2020-01-27 11:13:14 +0530

    Update get.js

--- routes/admin/allocate/mapping_supervisor/get.js
+++ routes/admin/allocate/mapping_supervisor/get.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../../db");
-const verifyToken = require("../verifyToken");
+const db = require("../../../db");
+const verifyToken = require("../../verifyToken");
 
 router.get("/", verifyToken, (req, res) => {
   let sql =
-    "SELECT society.society_id,society.society_name,centre.centre_id,centre_name,centre_pincode from society JOIN centre ON society.centre_id=centre.centre_id";
+    "SELECT mapping_supervisor.map_sup_id,mapping_supervisor.admin_id,admin.mobile_no,admin.name,admin.address_1,address_2,admin.city,admin.admin_type,mapping_supervisor.date_from,mapping_supervisor.date_to,centre.centre_name,centre_pincode from mapping_supervisor JOIN admin ON mapping_supervisor.admin_id=admin.admin_id JOIN centre ON mapping_supervisor.centre_id=centre.centre_id";
   let query = db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
     } else {
-      res.json({ societyget: result });
+      res.json({ mappingsupervisorget: result });
     }
   });
 });
