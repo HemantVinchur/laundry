70871f9af0c87efa6b9c48aababe4d9f129654a4
Omkar Lanjekar <52631363+lanomkar@users.noreply.github.com>
2020-01-27 11:25:03 +0530

    Update add.js

--- routes/admin/rate/add.js
+++ routes/admin/rate/add.js
@@ -6,15 +6,13 @@ router.post("/", verifyToken, (req, res) => {
   if (req.decoded.admin_type != "admin") {
     return res.json({ msg: "Unauthorized" });
   }
-  let { cloth_type_id, society_id, rate } = req.body;
-  if (cloth_type_id && society_id && rate) {
-    let post = {
-      cloth_type_id: cloth_type_id,
-      society_id: society_id,
-      rate: rate
-    };
-    let sql = "INSERT INTO rate SET ?";
-    let query = db.query(sql, post, (err, result) => {
+  let bulkclothtype = req.body.bulkclothtype;
+  let clothtype = Object.keys(req.body.bulkclothtype[0]);
+  let values = bulkclothtype.map(obj => clothtype.map(type => obj[type]));
+
+  if (values.length > 0) {
+    let sql = `INSERT INTO rate (${clothtype.join(",")}) values ?`;
+    let query = db.query(sql, [values], (err, result) => {
       if (err) {
         console.log(err);
       } else {
