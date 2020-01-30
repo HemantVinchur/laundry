72fd5aa3351a55c3b512902a9e4faa4dbadb8d6b
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-30 20:21:54 +0530

    Create getStandardTime.js

---
+++ routes/collectionBoy/getStandardTime.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const db = require("../db");
+const verifyToken = require("./../admin/verifyToken");
+const router = express.Router();
+
+router.get("/",verifyToken,(req,res)=>{
+  let date = new Date();
+  let timeValue=date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+  let getStandardTimeQuery=`select customer_name,phone,society_name,wings_name,flat_no from customer 
+  inner join customer_prefrence on customer.customer_id=customer_prefrence.customer_id 
+  inner join society on customer.society_id=society.society_id 
+  inner join wings on customer.wings_id=wings.wings_id 
+  where customer_prefrence.time_to>='${timeValue}' and society.society_id in (select society_id from mapping_collectionboy where admin_id='${req.decoded.admin_id}')`
+  
+  db.query(getStandardTimeQuery,(standardTimeResult,err)=>{
+    if(err)
+    {
+      console.log(err);
+      return res.status(500).json({ error: err.message , success: false});
+    }
+    else{
+      if(standardTimeResult.length>0)
+      {
+        return res.status(200).json({ success: true, message: "List of Customers",result:standardTimeResult });
+      }
+      else{
+          return res.status(200).json({ success: true, message: "No customer at this time" });
+      }
+    }
+  })
+})
