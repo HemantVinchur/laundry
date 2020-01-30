39d4ab40e9d674dc6ba62d07a8cf7a6006a83c6f
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-01-30 14:38:53 +0530

    Update checkForDelivery.js

--- routes/customer/checkForDelivery.js
+++ routes/customer/checkForDelivery.js
@@ -4,6 +4,7 @@ const db = require("../db");
 const router = express.Router();
 router.post("/", (request, response) => {
   let query = `SELECT status FROM orders WHERE customer_id='${request.body.customer_id}' AND status='9'`;
+  console.log(query);
   db.query(query, (err, res) => {
     if (err != null) return response.status(500).json({ error: err.message, success: false });
     //         console.log(res);
