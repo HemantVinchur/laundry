39ec44ecba8171b833f0105ee9cd7b3b803b906a
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-02-22 16:43:07 +0530

    Update getStatus.js

--- routes/customer/getStatus.js
+++ routes/customer/getStatus.js
@@ -4,9 +4,9 @@ const router = express.Router();
 router.post("/",(request,response)=>
 {
     let query=`SELECT total_count AS count,status,picked_date,status_desc,timestamp
-               FROM orders INNER JOIN status ON orders.status=status.status_id
-               INNER JOIN time ON orders.order_id=time.order_id
-               WHERE orders.customer_id='${request.body.customer_id}' AND NOT orders.status='14'`
+               FROM orders INNER JOIN time ON orders.order_id=time.order_id
+               INNER JOIN status ON time.status_id=status.status_id
+               WHERE orders.customer_id='${request.body.customer_id}' AND NOT orders.status='14' AND time.status_id=orders.status_id`
     db.query(query,(err,res)=>
     {
         if (err != null) return response.status(500).json({ error: err.message,success:false });
