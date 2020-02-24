272bd706cbb52ea5a709dc46faff9a2d5924c970
Omkar Lanjekar <52631363+lanomkar@users.noreply.github.com>
2020-02-24 17:20:48 +0530

    Update getSocietyUser.js

--- routes/admin/getSocietyUser.js
+++ routes/admin/getSocietyUser.js
@@ -5,7 +5,7 @@ const verifyToken = require("./verifyToken");
 
 router.get("/:collid", verifyToken, (req, res) => {
   var collid = req.params.collid;
-  let sql = `SELECT customer.customer_id,customer.customer_name from customer INNER JOIN society ON society.society_id=customer.society_id INNER JOIN mapping_collectionboy ON mapping_collectionboy.centre_id=society.centre_id WHERE admin_id=${collid} `;
+  let sql = `SELECT customer.customer_id,customer.customer_name from customer INNER JOIN mapping_collectionboy ON mapping_collectionboy.society_id=customer.society_id  WHERE admin_id=${collid} `;
   let query = db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
