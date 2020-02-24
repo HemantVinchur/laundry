bc1c01515881121f9c6f90cf7749426cf823ef5c
Omkar Lanjekar <52631363+lanomkar@users.noreply.github.com>
2020-02-24 17:03:33 +0530

    Update get.js

--- routes/admin/help/get.js
+++ routes/admin/help/get.js
@@ -7,7 +7,8 @@ router.get("/", verifyToken, (req, res) => {
   if (req.decoded.admin_type != "admin") {
     return res.json({ msg: "Unauthorized" });
   }
-  let sql = "SELECT help_id,customerid,	helptype,helpbrief FROM help";
+  let sql =
+    "SELECT help_id,customerid,customer.customer_name,customer.flat_no,customer.phone,society.society_name,wings.wings_name,	helptype,helpbrief FROM help INNER JOIN customer ON customer.customer_id=help.customerid INNER JOIN society ON society.society_id=customer.society_id INNER JOIN wings ON wings.wings_id=customer.wings_id";
   let query = db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
