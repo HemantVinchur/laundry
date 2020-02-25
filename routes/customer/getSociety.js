140852abf5c58a3b2fd4e2a78059b2b5c9f9304b
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-02-25 19:31:54 +0530

    Update getSociety.js

--- routes/customer/getSociety.js
+++ routes/customer/getSociety.js
@@ -16,4 +16,19 @@ router.post("/",(request,response)=>
     });
 
 });
+router.get("/",(request,response)=>
+{
+    let society=`SELECT society_id,society_name FROM society`;
+    db.query(society,(err,res)=>
+    {
+        if (err != null) return response.status(500).json({ error: err.message, success:false});
+        if(res.length>0){
+        return response.status(200).json({result:res,success:true});
+        }
+        else{
+            return response.json({message:"Society Doesn't Exist",success:true});
+        }
+    });
+
+});
 module.exports=router;
