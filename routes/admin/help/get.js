fd8da3f9806a224500c32a9edfbf80b68e6c27d7
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-03 18:55:09 +0530

    Update get.js

--- routes/admin/help/get.js
+++ routes/admin/help/get.js
@@ -8,7 +8,7 @@ router.get("/", verifyToken, (req, res) => {
     return res.json({ msg: "Unauthorized" });
   }
   let sql =
-    "SELECT help_id,customerid,customer.customer_name,customer.flat_no,customer.phone,society.society_name,wings.wings_name,	helptype,helpbrief,help_reply.helpreply FROM help INNER JOIN customer ON customer.customer_id=help.customerid INNER JOIN society ON society.society_id=customer.society_id INNER JOIN wings ON wings.wings_id=customer.wings_id LEFT OUTER JOIN help_reply on help.help_id=help_reply.help_id";
+    "SELECT help.help_id,customerid,customer.customer_name,customer.flat_no,customer.phone,society.society_name,wings.wings_name,	helptype,helpbrief,help_reply.helpreply FROM help INNER JOIN customer ON customer.customer_id=help.customerid INNER JOIN society ON society.society_id=customer.society_id INNER JOIN wings ON wings.wings_id=customer.wings_id LEFT OUTER JOIN help_reply on help.help_id=help_reply.help_id";
   let query = db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
