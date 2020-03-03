a7d4ba28ac5da805fb326f4f0c5fe28ecf9f1df9
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-03 21:43:48 +0530

    Update getRate.js

--- routes/customer/getRate.js
+++ routes/customer/getRate.js
@@ -6,7 +6,7 @@ router.post("/",(request,response)=>
     let query=`SELECT cloth_type,rate,discount 
                FROM rate INNER JOIN cloth_type
                ON rate.cloth_type_id=cloth_type.cloth_type_id LEFT OUTER JOIN discount on rate.society_id=discount.society_id
-               WHERE society_id in (select society_id from customer where customer_id='${request.body.customer_id}')`;
+               WHERE rate.society_id in (select society_id from customer where customer_id='${request.body.customer_id}')`;
     db.query(query,(err,res)=>{
         if (err)
         {
