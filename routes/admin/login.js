35343bf7c3c979016893c0f22a24c34743421500
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-04 12:11:27 +0530

    Update login.js

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -35,14 +35,17 @@ router.post("/", (req, res) => {
               );
               res.header("auth-token", token).json({ tokenkey: token,type:result[0].admin_type,success:true });
             } else {
-              res.json({ msg: "Wrong password" });
+              res.json({ success:false,msg: "Incorrect Username/Password" });
             }
           });
           //Create and assign  a token
         } else {
           res.json({ msg: "mobile no  doesnot exist" });
         }
-      }
+	  }
+	  else{
+		res.json({ success:false,msg: "Incorrect Username/Password" });
+	  }
     });
   } else {
     res.json({ msg: "Enter all fields" });
