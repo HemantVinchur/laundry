1a867d1bcab6bd7ea013c5904586307ba6126d71
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-05 12:54:27 +0530

    Update login.js

--- routes/collectionBoy/login.js
+++ routes/collectionBoy/login.js
@@ -24,6 +24,10 @@ router.post("/", (req, res) => {
         if (result[0].admin_id > 0) {
           bcrypt.compare(password, result[0].password, function(err, res1) {
             // res === true
+            if(err)
+            {
+              console.log(err);
+            }
             if (res1) {
               const token = jwt.sign(
                 {
@@ -31,9 +35,7 @@ router.post("/", (req, res) => {
                 },
                 "TOKENSECRETFORADMIN"
               );
-              res
-                .header("auth-token", token)
-                .json({
+              res.header("auth-token", token).json({
                   tokenkey: token,
                   success: true,
                   type: result[0].admin_type
