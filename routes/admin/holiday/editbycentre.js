e5d67a0e835eefc99f35d563bdb79b9e92697f7b
Omkar Lanjekar <52631363+lanomkar@users.noreply.github.com>
2020-03-05 14:28:15 +0530

    Update editbycentre.js

--- routes/admin/holiday/editbycentre.js
+++ routes/admin/holiday/editbycentre.js
@@ -9,7 +9,7 @@ router.put("/", verifyToken, (req, res) => {
     let update = {
       holiday_date:holiday_date
     };
-    let sql = `UPDATE holiday  SET ?  WHERE centre_id='${centre_id} and holiday_id=${holiday_id}'`;
+    let sql = `UPDATE holiday  SET ?  WHERE centre_id=${centre_id} and holiday_id=${holiday_id}`;
     let query = db.query(sql, update, (err, result) => {
       if (err) {
         console.log(err);
