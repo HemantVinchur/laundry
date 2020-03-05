ae43ad522e59282e407a8892fafb9406e98f5e82
Omkar Lanjekar <52631363+lanomkar@users.noreply.github.com>
2020-03-05 14:03:15 +0530

    Update holiday.js

--- routes/admin/holiday/holiday.js
+++ routes/admin/holiday/holiday.js
@@ -7,4 +7,10 @@ router.use("/add", add);
 var get = require("./get");
 router.use("/get", get);
 
+var getbycentre = require("./getbycentre");
+router.use("/getbycentre",getbycentre);
+
+var editbycentre = require("./editbycentre");
+router.use("/editbycentre",editbycentre);
+
 module.exports = router;
