78ec0d13a8e11ccec6b81bd025e4cc20771d3b2f
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-15 18:27:20 +0530

    Update addStandardTime.js

--- routes/customer/addStandardTime.js
+++ routes/customer/addStandardTime.js
@@ -3,7 +3,7 @@ const db = require("../db");
 const router = express.Router();
 
 router.post("/",(req,res)=>{
-  let addStandardQuery=`insert into customer_prefrence(customer_id,time_from,time_to) values('${req.body.customer_id}','${req.body.time_from}','${req.body.time_to}')`
+  let addStandardQuery=`insert into customer_prefrence(customer_id,time_from,time_to) values('${req.body.customer_id}','${req.body.time_from}','${req.body.time_to}');update customer set prefered_time=1 where customer_id='${req.body.customer_id}'`
   
   db.query(addStandardQuery,(err,resultAddStandard)=>{
     if(err)
