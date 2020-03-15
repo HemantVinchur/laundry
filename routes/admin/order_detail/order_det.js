0be64bbea4a9a053241bbb3ddb83ee58d68823aa
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-15 19:10:11 +0530

    Update order_det.js

--- routes/admin/order_detail/order_det.js
+++ routes/admin/order_detail/order_det.js
@@ -5,7 +5,7 @@ const verifyToken = require("../verifyToken");
 
 router.get("/:status", verifyToken, (req, res) => {
   let sql =
-    `SELECT  orders.order_id,customer.customer_name,customer.phone,customer.flat_no,orders.picked_date,orders.total_count,society.society_name,wings.wings_name ,(select max(timestamp) from time where time.order_id=orders.order_id)picked_date  FROM  orders, customer,society,wings where orders.customer_id=customer.customer_id && customer.society_id=society.society_id && customer.wings_id=wings.wings_id && status="${req.params.status}"`;
+    `SELECT  orders.order_id,customer.customer_name,customer.phone,customer.flat_no,orders.picked_date,orders.total_count,society.society_name,wings.wings_name ,(select max(timestamp) from time where time.order_id=orders.order_id)picked_date  FROM  orders, customer,society,wings where orders.customer_id=customer.customer_id && customer.society_id=society.society_id && customer.wings_id=wings.wings_id && status IN ("${req.params.status}")`;
   let query = db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
