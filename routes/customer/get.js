2fc7b9157f05b4b52b2605bde1ec726938bad4e6
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-16 10:52:19 +0530

    Update get.js

--- routes/customer/get.js
+++ routes/customer/get.js
@@ -2,13 +2,13 @@ const express = require("express");
 const db = require("../db");
 const router = express.Router();
 router.post("/",(request,response)=>
-{   
+{   //SELECT time_from,time_to from customer_prefrence where customer_id='${id}'
     let id=request.body.customer_id;
-    let query=`SELECT customer_name,society_name AS society,wings_name AS wing, flat_no , phone,prefered_time
+    let query=`SELECT customer_name,society_name AS society,wings_name AS wing, flat_no , phone
                FROM customer 
                INNER JOIN society ON customer.society_id=society.society_id 
                INNER JOIN wings ON wings.society_id=society.society_id
-               WHERE customer.customer_id='${id}';SELECT time_from,time_to from customer_prefrence where customer_id='${id}'`;
+               WHERE customer.customer_id='${id}'`;
     db.query(query,(err,res)=>{
         if (err != null) return response.status(500).json({ error: err.message , success:false});
 //         console.log(res);
