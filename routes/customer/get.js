4cd8591ba4b7615b84300b851453c6ac0b4ea40e
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-16 11:02:31 +0530

    Update get.js

--- routes/customer/get.js
+++ routes/customer/get.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post("/",(request,response)=>
 {   //SELECT time_from,time_to from customer_prefrence where customer_id='${id}'
     let id=request.body.customer_id;
-    let query=`SELECT customer_name,society_name AS society,wings_name AS wing, flat_no , phone
+    let query=`SELECT customer_name,society_name AS society,wings_name AS wing, flat_no , phone,prefered_time
                FROM customer 
                INNER JOIN society ON customer.society_id=society.society_id 
                INNER JOIN wings ON wings.wings_id=customer.wings_id
