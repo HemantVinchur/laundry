1388a62e45da16af21aab7ccb3b9b63c023eeec4
ESLABS101 <40630231+ESLABS101@users.noreply.github.com>
2020-03-18 14:38:48 +0530

    Update getCustomers.js

--- routes/admin/getCustomers.js
+++ routes/admin/getCustomers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require("../db");
 const verifyToken = require("./verifyToken");
 
-router.get('/getCustomers',verifyToken,(req,res)=>{
+router.get('/',verifyToken,(req,res)=>{
     let query=`select customer_id,customer_name,phone,flat_no,wings_name,society_name from customer inner join society on customer.society_id = society.society_id inner join wings on customer.wings_id = wings.wings_id;`
     db.query(query,(err,result)=>{
         if(err)
